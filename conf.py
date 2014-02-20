be0c36b060361618f087266bcf616dab765cfc1b
goosey15@gmail.com <goosey15@gmail.com@4cd0485c-7a45-ac1c-090d-d5cdff30d0a9>
2014-02-20 19:02:30 +0000

    git-svn-id: https://py-auth-server.googlecode.com/svn/trunk@310 4cd0485c-7a45-ac1c-090d-d5cdff30d0a9

--- conf.py
+++ conf.py
@@ -83,7 +83,7 @@ def fake_imports():
     for mod_name in MOCK_MODULES:
         sys.modules[mod_name] = Mock()
 
-
+fake_imports()
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
