d5fda9c5f24d2c178e5e1e6cfe09f717e8a2fb8e
goosey15@gmail.com <goosey15@gmail.com@4cd0485c-7a45-ac1c-090d-d5cdff30d0a9>
2014-02-20 17:14:33 +0000

    git-svn-id: https://py-auth-server.googlecode.com/svn/trunk@301 4cd0485c-7a45-ac1c-090d-d5cdff30d0a9

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='network',
+      version='0.1',
+      description="My sample package",
+      long_description="",
+      author='TODO',
+      author_email='todo@example.org',
+      license='TODO',
+      packages=['network'],
+      zip_safe=False,
+      install_requires=[
+          # 'Sphinx',
+          # ^^^ Not sure if this is needed on readthedocs.org
+          # 'something else?',
+          ],
+      )
