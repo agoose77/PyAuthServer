b0bd1056bc1cd691facc7115561540f99eb8264b
goosey15@gmail.com <goosey15@gmail.com@4cd0485c-7a45-ac1c-090d-d5cdff30d0a9>
2014-02-20 18:00:42 +0000

    git-svn-id: https://py-auth-server.googlecode.com/svn/trunk@304 4cd0485c-7a45-ac1c-090d-d5cdff30d0a9

--- setup.py
+++ setup.py
@@ -1,13 +1,43 @@
 from setuptools import setup
 
 setup(name='network',
-      version='0.1',
-      description="My sample package",
+      version='1.0',
+      description="Network package",
       long_description="",
       author='TODO',
       author_email='todo@example.org',
       license='TODO',
       packages=['network'],
+      zip_safe=False,
+      install_requires=[
+          # 'Sphinx',
+          # ^^^ Not sure if this is needed on readthedocs.org
+          # 'something else?',
+          ],
+      )
+setup(name='bge_network',
+      version='1.0',
+      description="BGE network package",
+      long_description="",
+      author='TODO',
+      author_email='todo@example.org',
+      license='TODO',
+      packages=['bge_network'],
+      zip_safe=False,
+      install_requires=[
+          # 'Sphinx',
+          # ^^^ Not sure if this is needed on readthedocs.org
+          # 'something else?',
+          ],
+      )
+setup(name='blender_example',
+      version='1.0',
+      description="Blender Example package",
+      long_description="",
+      author='TODO',
+      author_email='todo@example.org',
+      license='TODO',
+      packages=['blender_example'],
       zip_safe=False,
       install_requires=[
           # 'Sphinx',
