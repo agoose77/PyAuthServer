55050b3bc609e7a614e6230e8675ebea627cf93e
agoose77 <goosey15@gmail.com>
2014-05-20 00:20:41 +0100

    Removed switched context manager (unused) Cleaned Enum mapping - now provides keys_to_values and values_to_keys dictionaries

--- network/enums.py
+++ network/enums.py
@@ -5,10 +5,10 @@ __all__ = ['Enumeration', 'ConnectionStatus', 'Netmodes', 'Protocols', 'Roles']
 
 class Enumeration(type):
     """Metaclass for Enumerations in Python"""
+
     def __new__(cls, name, parents, attrs):
         # Get settings
-        get_index = (lambda x: 2 ** x if attrs.get('use_bits', False)
-                     else x)
+        get_index = (lambda x: 2 ** x if attrs.get('use_bits', False) else x)
 
         values = attrs['values']
 
@@ -16,22 +16,22 @@ class Enumeration(type):
         reverse_mapping = {i: v for v, i in forward_mapping.items()}
 
         attrs.update(forward_mapping)
-        attrs['reverse'] = reverse_mapping
+        attrs['keys_to_values'] = forward_mapping
+        attrs['values_to_keys'] = reverse_mapping
 
         # Return new class
         return super().__new__(cls, name, parents, attrs)
 
     def __getitem__(self, value):
         # Add ability to lookup name
-        return self.reverse[value]
+        return self.values_to_keys[value]
 
     def __contains__(self, index):
-        return index in self.reverse
+        return index in self.values_to_keys
 
     def __repr__(self):
-        return "<Enumeration {}>\n{}\n".format(self.__name__,
-                                 '\n'.join("<{}: {}>".format(n, v)
-                                   for v, n in self.reverse.items()))
+        contents_string = '\n'.join("<{}: {}>".format(*mapping) for mapping in self.keys_to_values.items())
+        return "<Enumeration {}>\n{}\n".format(self.__name__, contents_string)
 
 
 class ConnectionStatus(metaclass=Enumeration):
@@ -47,14 +47,12 @@ class HandshakeState(metaclass=Enumeration):
 
 
 class Protocols(metaclass=Enumeration):
-    values = ("request_disconnect", "request_handshake",
-              "replication_init", "replication_del",
-              "replication_update", "method_invoke")
+    values = ("request_disconnect", "request_handshake", "replication_init", "replication_del",  "replication_update",
+              "method_invoke")
 
 
 class Roles(metaclass=Enumeration):
-    values = ("none", "dumb_proxy", "simulated_proxy",
-              "autonomous_proxy", "authority")
+    values = ("none", "dumb_proxy", "simulated_proxy", "autonomous_proxy", "authority")
 
     __slots__ = "local", "remote", "context"
 
@@ -67,23 +65,20 @@ class Roles(metaclass=Enumeration):
         return hash((self.context, self.local, self.remote))
 
     def __repr__(self):
-        return "Roles: Local: {}, Remote: {}".format(
-                                                 self.__class__[self.local],
-                                                 self.__class__[self.remote])
+        return "Roles: Local: {}, Remote: {}".format(self.__class__[self.local], self.__class__[self.remote])
 
     @contextmanager
     def set_context(self, owner):
         self.context = owner
+
         switched = self.remote == Roles.autonomous_proxy and not owner
+
         if switched:
             self.remote = Roles.simulated_proxy
+
         yield
+
         if switched:
             self.remote = Roles.autonomous_proxy
-        self.context = None
 
-    @contextmanager
-    def switched(self):
-        self.remote, self.local = self.local, self.remote
-        yield
-        self.remote, self.local = self.local, self.remote
+        self.context = None
