69e10b294cf6396d0b19cdcaa3633efe09210598
agoose77 <goosey15@gmail.com>
2014-05-28 01:47:45 +0100

    Updated logger style

--- network/logger.py
+++ network/logger.py
@@ -1,4 +1,4 @@
-from logging import Formatter, getLogger, INFO, StreamHandler
+from logging import Formatter, getLogger, INFO, DEBUG, StreamHandler
 
 __all__ = ['logger']
 
@@ -10,7 +10,7 @@ logger.info("Initialised logger")
 _handler = StreamHandler()
 _handler.setLevel(INFO)
 # create formatter and add it to the handlers
-formatter = Formatter('%(levelname)s - [%(asctime)s - %(name)s]\n{\n\t%(message)s\n}')
+formatter = Formatter('%(levelname)s - [%(asctime)s - %(name)s] {%(message)s\}')
 _handler.setFormatter(formatter)
 
 logger.addHandler(_handler)
