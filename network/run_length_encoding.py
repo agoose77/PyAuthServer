5fa3691f2d577a7e66cc76cee7271af0431db5e6
agoose77 <goosey15@gmail.com>
2014-06-18 00:09:11 +0100

    Updated documentation

--- network/run_length_encoding.py
+++ network/run_length_encoding.py
@@ -7,9 +7,19 @@ class RunLengthCodec:
     """RLE compression codec"""
 
     @staticmethod
-    def encode(values):
-        return [(len(list(group)), key) for key, group in groupby(values)]
+    def encode(sequence):
+        """Apply run length encoding to a sequence
+
+        Returns a list of (count, item) pairs
+        :param sequence: sequence of values to encode
+        """
+        return [(len(list(group)), key) for key, group in groupby(sequence)]
 
     @staticmethod
-    def decode(values):
-        return [key for (length, key) in values for _ in range(length)]
+    def decode(sequence):
+        """Parse run length encoding from a sequence
+
+        Returns original sequence as a list
+        :param sequence: sequence of value pairs to decode
+        """
+        return [key for (length, key) in sequence for _ in range(length)]
