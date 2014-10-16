2e5d1ae171f6f5e57ccd681d68a25020e98d35c1
agoose77 <goosey15@gmail.com>
2014-10-16 02:06:23 +0100

    Another docstring

---
+++ network/rules.py
@@ -0,0 +1,17 @@
+__all__ = ['ReplicationRulesBase']
+
+
+class ReplicationRulesBase:
+    """Base class for replication rules"""
+
+    def pre_initialise(self, addr, netmode):
+        raise NotImplementedError
+
+    def post_initialise(self, replication_stream):
+        raise NotImplementedError
+
+    def post_disconnected(self, replication_stream, replicable):
+        raise NotImplementedError
+
+    def is_relevant(self, conn, replicable):
+        raise NotImplementedError
