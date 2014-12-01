01c29328c4602884f1d7bb4b1274074043e779f5
Angus <goosey15@gmail.com>
2014-12-01 16:24:27 +0000

    Removed unused imports

--- game_system/gameloop.py
+++ game_system/gameloop.py
@@ -1,11 +1,5 @@
-from collections import namedtuple, OrderedDict
+from collections import namedtuple
 
-from network.enums import Netmodes
-from network.network import Network
-from network.signals import DisconnectSignal
-from network.world_info import WorldInfo
-
-from .timer import Timer
 from .tagged_delegate import EnvironmentDefinitionByTag
 
 
