aa887ee6298984020d38f474445276eb52c16d6c
agoose77 <goosey15@gmail.com>
2015-02-02 14:31:45 +0000

    Removed duplicate iterator Extended Bidirectional Iterator functionality

--- network/iterators.py
+++ network/iterators.py
@@ -25,37 +25,6 @@ class RenewableGenerator:
             return next(self._internal)
 
 
-class BidirectionalIterator:
-    """Two directional iterator
-
-    Converts sequence to tuple internally
-    """
-
-    def __init__(self, collection):
-        self.collection = tuple(collection)
-        self.index = -1
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        try:
-            self.index += 1
-            result = self.collection[self.index]
-
-        except IndexError:
-            raise StopIteration
-
-        return result
-
-    def __prev__(self):
-        self.index -= 1
-        if self.index < 0:
-            raise StopIteration
-
-        return self.collection[self.index]
-
-
 def take_single(iterable, default=None, reverse=False):
     """Returns first element from iterable
 
