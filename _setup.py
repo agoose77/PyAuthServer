e24dfc3360fd48219cd55701da81f038d7cd9706
agoose77 <goosey15@gmail.com>
2015-02-02 18:51:26 +0000

    Documentation

---
+++ _setup.py
@@ -0,0 +1,62 @@
+from setuptools import setup
+
+setup(name='network',
+      version='1.0.1',
+      description="Network package",
+      long_description="",
+      author='Angus Hollands',
+      author_email='goosey15@gmail.com',
+      license='MIT',
+      packages=['network'],
+      zip_safe=False,
+      install_requires=[
+          # 'Sphinx',
+          # ^^^ Not sure if this is needed on readthedocs.org
+          # 'something else?',
+          ],
+      )
+setup(name='bge_game_system',
+      version='1.0.1',
+      description="BGE network package",
+      long_description="",
+      author='Angus Hollands',
+      author_email='goosey15@gmail.com',
+      license='MIT',
+      packages=['bge_game_system'],
+      zip_safe=False,
+      install_requires=[
+          # 'Sphinx',
+          # ^^^ Not sure if this is needed on readthedocs.org
+          # 'something else?',
+          ],
+      )
+setup(name='game_system',
+      version='1.0.1',
+      description="Game System package",
+      long_description="",
+      author='Angus Hollands',
+      author_email='goosey15@gmail.com',
+      license='TODO',
+      packages=['game_system'],
+      zip_safe=False,
+      install_requires=[
+          # 'Sphinx',
+          # ^^^ Not sure if this is needed on readthedocs.org
+          # 'something else?',
+          ],
+      )
+setup(name='demos',
+      version='1.0.1',
+      description="Demo package",
+      long_description="",
+      author='Angus Hollands',
+      author_email='goosey15@gmail.com',
+      license='TODO',
+      packages=['demos'],
+      zip_safe=False,
+      install_requires=[
+          # 'Sphinx',
+          # ^^^ Not sure if this is needed on readthedocs.org
+          # 'something else?',
+          ],
+      )
