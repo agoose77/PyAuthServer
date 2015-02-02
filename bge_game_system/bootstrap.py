c7ddd4217ca0452819f65d3d136531ddf3b93fcb
agoose77 <goosey15@gmail.com>
2015-02-02 20:30:18 +0000

    Docstring for bootstrap

--- bge_game_system/bootstrap.py
+++ bge_game_system/bootstrap.py
@@ -1,3 +1,7 @@
+"""Import before instantiation of runtime-resolved types in game_system module.
+
+Sets the data path and environment variables for Blender Game Engine
+"""
 from bge import logic
 from game_system.resources import ResourceManager
 
