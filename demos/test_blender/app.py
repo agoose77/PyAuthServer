ba04a395b2e0f4654cf9c7504ff3bcd3247339d5
Angus Hollands <goosey15@gmail.com>
2015-05-15 18:22:14 +0100

    Add missing files

---
+++ demos/test_blender/app.py
@@ -0,0 +1,84 @@
+try:
+    import bge
+
+except ImportError:
+    try:
+        from blender_game_system.game_loop import Client, Server, register
+        register()
+    except ImportError:
+        from panda_game_system.game_loop import Client, Server
+
+else:
+    from bge_game_system.game_loop import Client, Server
+
+from network.connection import Connection
+from network.world_info import WorldInfo
+from network.rules import ReplicationRulesBase
+from network.enums import Netmodes
+
+from game_system.controllers import PawnController
+from game_system.clock import Clock
+from game_system.entities import Actor
+from game_system.replication_info import ReplicationInfo
+
+from .actors import *
+from .controllers import TestPandaPlayerController
+
+
+classes = dict(server=Server, client=Client)
+
+
+class Rules(ReplicationRulesBase):
+
+    def pre_initialise(self, addr, netmode):
+        return
+
+    def post_disconnect(self, conn, replicable):
+        replicable.deregister()
+
+    def post_initialise(self, replication_stream):
+        cont = TestPandaPlayerController()
+        cont.possess(TestActor())
+        return cont
+
+    def is_relevant(self, connection, replicable):
+        if isinstance(replicable, PawnController):
+            return False
+
+        elif isinstance(replicable, (Actor, ReplicationInfo, Clock)):
+            return True
+
+        elif replicable.always_relevant:
+            return True
+
+
+def init_game():
+    if WorldInfo.netmode == Netmodes.server:
+        floor = TestActor()
+        floor.transform.world_position = [0, 30, -1]
+
+     #   floor.physics.mass = 0.0
+     #   floor.mass = 0.0
+
+    else:
+        Connection.create_connection("localhost", 1200)
+
+WorldInfo.rules = Rules()
+
+def run(mode):
+    try:
+        cls = classes[mode]
+
+    except KeyError:
+        print("Unable to start {}".format(mode))
+        return
+
+    if mode == "server":
+        pass
+    else:
+        WorldInfo.netmode = Netmodes.client
+
+    game_loop = cls()
+    init_game()
+    game_loop.delegate()
+    del game_loop
---
+++ demos/test_blender/controllers.py
@@ -0,0 +1,62 @@
+from network.descriptors import TypeFlag
+from network.rpc import Pointer
+from network.decorators import requires_netmode
+from network.enums import Netmodes
+from network.world_info import WorldInfo
+
+from game_system.controllers import PlayerPawnController
+from game_system.coordinates import Vector
+from game_system.enums import ButtonState
+from game_system.inputs import InputContext
+
+from .actors import TestActor
+
+
+class TestPandaPlayerController(PlayerPawnController):
+    input_context = InputContext(buttons=["left", "right", "up", "down", "debug"])
+
+    debug = False
+
+    @requires_netmode(Netmodes.server)
+    def shoot(self):
+        cube = TestActor()
+        pawn = self.pawn
+        cube.transform.world_position = pawn.transform.world_position + Vector((-5, 0, 0))
+
+    def process_inputs(self, buttons, ranges):
+        if buttons['debug'] == ButtonState.pressed:
+            self.shoot()
+            #self.debug = not self.debug
+
+        if self.debug:
+            self.shoot()
+            # print(self.buffer)
+            # print(buttons)
+
+        y_sign = 0
+        if buttons['up'] in {ButtonState.pressed, ButtonState.held}:
+            y_sign += 1
+
+        if buttons['down'] in {ButtonState.pressed, ButtonState.held}:
+            y_sign -= 1
+
+        x_sign = 0
+        if buttons['right'] in {ButtonState.pressed, ButtonState.held}:
+            x_sign -= 1
+
+        if buttons['left'] in {ButtonState.pressed, ButtonState.held}:
+            x_sign += 1
+
+        y_speed = y_sign * 2.0
+        rotation_speed = x_sign
+
+        pawn = self.pawn
+        if pawn is None:
+            return
+        velocity = Vector((0.0, y_speed, 0.0))
+        velocity.rotate(pawn.transform.world_orientation)
+
+        angular = Vector((0.0, 0.0, rotation_speed))
+
+        pawn.physics.world_angular = angular
+        pawn.physics.world_velocity = velocity
